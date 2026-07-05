de5b33600428df42c35b11c82fc749fb8eb7d0d6
sedin-pavithrar <pavithra.r@sedintechnologies.com>
2026-07-05 18:41:16 +0530

    created index.html , replaced index() with render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
 def index(request):
-    return HttpResponse("Hello Viewer , You'r at the polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5] #querying the db 
+    #question.objects -> question table slice 5 -newest first 
+
+    context = {
+        "latest_question_list": latest_question_list
+    } 
+
+    return render(request,"polls/index.html",context)
+   # render needs three things 
+   # Request - > who asked 
+   # Template -> Which HTML file 
+   # Context -> What data should appear 
+
+    # return HttpResponse("Hello Viewer , You'r at the polls index")
 
 def detail(request,question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
